e3700e7f5706deae645d1c1af3942b734c3758c6
Sundeep <sunnyr22@gmail.com>
2015-07-15 20:10:50 -0400

    Added mouse up mouse down

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	$(".ryu").mouseenter(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
+		
 	})
 	.mouseleave(function() {
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
-    // play hadouken sound
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').show();
